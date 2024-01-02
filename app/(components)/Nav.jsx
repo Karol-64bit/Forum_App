7da67641f0f284646c7395eeca29ef4ab994831f
Karol <67333775+Karol-64bit@users.noreply.github.com>
2024-01-02 19:28:57 +0100

    init

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      m-crap@crappysoft.com
+      r-conner@crappysoft.com
+     
+    </div>
+  )
+}
+
+export default Nav
---
+++ app/CreateUser/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Only Admins!</h1>
+    </div>
+  )
+}
+
+export default page
